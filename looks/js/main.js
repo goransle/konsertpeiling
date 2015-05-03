ee1aae5b564801cc3f45416965f8aeb7191f15e5
kvalheimaren <mr_kvalheim@hotmail.com>
2015-05-03 19:40:16 +0200

    tilbakesveip

---
+++ looks/js/main.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+    // TODO:: Do your initialization job
+
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName == "back")
+        	window.history.back();
+    });
+
+    // Sample code
+    var textbox = document.querySelector('.contents');
+    textbox.addEventListener("click", function(){
+    	box = document.querySelector('#textbox');
+    	box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
+    });
+    
+};
+
+
