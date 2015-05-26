3ec51c04cec8ea08b2130c76d323fda626260d46
goransle <goransle@gmail.com>
2015-05-26 19:14:21 +0200

    masse rart

--- js/main.js
+++ js/main.js
@@ -6,32 +6,11 @@ window.onload = function () {
 		if(e.keyName == "back")
 			window.history.back();
 	});
-
-	// Sample code
-	var textbox = document.querySelector('.contents');
-	textbox.addEventListener("click", function(){
-		box = document.querySelector('#textbox');
-		box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
+	function vibrer(){
+		navigator.vibrate(70);
+	}
+	$("a, button, input, tr, .button").click(function() {
+		vibrer();
+		console.log("vibrerer");
 	});
-
 }
-//multiple select box of genres
-jQuery.fn.multiselect = function() {
-	$(this).each(function() {
-		var checkboxes = $(this).find("input:checkbox");
-		checkboxes.each(function() {
-			var checkbox = $(this);
-			// Highlight pre-selected checkboxes
-			if (checkbox.prop("checked"))
-				checkbox.parent().addClass("multiselect-on");
-
-			// Highlight checkboxes that the user selects
-			checkbox.click(function() {
-				if (checkbox.prop("checked"))
-					checkbox.parent().addClass("multiselect-on");
-				else
-					checkbox.parent().removeClass("multiselect-on");
-			});
-		});
-	});
-};
